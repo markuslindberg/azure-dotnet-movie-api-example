179465281a328fa12cfc0cbc708045e253f9a9d9
Markus Lindberg <markus110@gmail.com>
2024-12-15 11:04:24 +0100

    Add app service authentication

--- infra/MoviesApi.Infra/Program.cs
+++ infra/MoviesApi.Infra/Program.cs
@@ -1,18 +1,55 @@
 ﻿using MoviesApi.Infra;
 using Pulumi;
+using Pulumi.AzureAD;
 using Pulumi.AzureNative.Resources;
 using Pulumi.AzureNative.Storage;
-using Pulumi.AzureNative.Storage.Inputs;
 using Pulumi.AzureNative.Web;
 using Pulumi.AzureNative.Web.Inputs;
 using AppInsights = Pulumi.AzureNative.Insights;
 
 return await Pulumi.Deployment.RunAsync(() =>
 {
+    var azureConfig = new Pulumi.Config("azure-native");
+    var tenantId = azureConfig.Require("tenantId");
+
     var resourceGroupName = Output.Create(GetResourceGroup.InvokeAsync(new GetResourceGroupArgs
     {
         ResourceGroupName = "rg-moviesapi"
     })).Apply(x => x.Name);
+
+    var appRegistration = new Application("appreg-moviesapi", new ApplicationArgs
+    {
+        DisplayName = "Movies Api",
+        Api = new Pulumi.AzureAD.Inputs.ApplicationApiArgs
+        {
+            Oauth2PermissionScopes = new[]
+            {
+                new Pulumi.AzureAD.Inputs.ApplicationApiOauth2PermissionScopeArgs
+                {
+                    Id = new Pulumi.Random.RandomUuid("appreg-moviesapi-scope-id").Id,
+                    Type = "User",
+                    Value = "user_impersonation",
+                    AdminConsentDescription = "Allow the application access on behalf of the signed-in user.",
+                    AdminConsentDisplayName = "Access Movies Api",
+                    UserConsentDescription = "Allow the application access on your behalf.",
+                    UserConsentDisplayName = "Access Movies Api",
+                    Enabled = true,
+                }
+            }
+        }
+    });
+
+    var appIdentifierUri = new ApplicationIdentifierUri("appreg-moviesapi-identifier-uri", new ApplicationIdentifierUriArgs
+    {
+        ApplicationId = appRegistration.Id,
+        IdentifierUri = Output.Format($"api://{appRegistration.ClientId}")
+    });
+
+    var appSecret = new ApplicationPassword("appreg-moviesapi-secret", new ApplicationPasswordArgs
+    {
+        ApplicationId = appRegistration.Id,
+        EndDate = DateTimeOffset.Parse("2025-01-01").AddDays(90).ToString("o"),
+    });
 
     var applicationInsights = new AppInsights.Component("appi-moviesapi", new()
     {
@@ -26,7 +63,7 @@ return await Pulumi.Deployment.RunAsync(() =>
     {
         ResourceGroupName = resourceGroupName,
         Kind = Kind.StorageV2,
-        Sku = new SkuArgs
+        Sku = new Pulumi.AzureNative.Storage.Inputs.SkuArgs
         {
             Name = SkuName.Standard_LRS
         },
@@ -53,26 +90,74 @@ return await Pulumi.Deployment.RunAsync(() =>
     {
         ResourceGroupName = resourceGroupName,
         ServerFarmId = appServicePlan.Id,
+        HttpsOnly = true,
         SiteConfig = new SiteConfigArgs
         {
-            AppSettings = new NameValuePairArgs[]
-            {
-                new ()
+            MinTlsVersion = "1.2",
+            AppSettings = [
+                new NameValuePairArgs()
                 {
                     Name = "StorageConnectionString",
-                    Value = OutputHelpers.StorageConnectionString(storageAccount, resourceGroupName),
+                    Value = OutputHelpers.StorageConnectionString(storageAccount, resourceGroupName)
                 },
-                new ()
+                new NameValuePairArgs()
                 {
                     Name = "APPLICATIONINSIGHTS_CONNECTION_STRING",
-                    Value = Output.Format($"InstrumentationKey={applicationInsights.InstrumentationKey}"),
+                    Value = Output.Format($"InstrumentationKey={applicationInsights.InstrumentationKey}")
                 },
-            },
+                new NameValuePairArgs()
+                {
+                    Name = "WEBSITE_AUTH_AAD_ALLOWED_TENANTS",
+                    Value = tenantId
+                },
+                new NameValuePairArgs()
+                {
+                    Name = "MICROSOFT_PROVIDER_AUTHENTICATION_SECRET",
+                    Value = appSecret.Value
+                },
+            ],
+        },
+    });
+
+    var authSettings = new WebAppAuthSettingsV2("app-moviesapi-auth", new WebAppAuthSettingsV2Args
+    {
+        Name = app.Name,
+        ResourceGroupName = resourceGroupName,
+        GlobalValidation = new GlobalValidationArgs
+        {
+            RequireAuthentication = true,
+            UnauthenticatedClientAction = UnauthenticatedClientActionV2.Return401,
+        },
+        Platform = new AuthPlatformArgs
+        {
+            Enabled = true,
+        },
+        IdentityProviders = new IdentityProvidersArgs
+        {
+            AzureActiveDirectory = new AzureActiveDirectoryArgs
+            {
+                Enabled = true,
+                Registration = new AzureActiveDirectoryRegistrationArgs
+                {
+                    ClientId = appRegistration.ClientId,
+                    ClientSecretSettingName = "MICROSOFT_PROVIDER_AUTHENTICATION_SECRET",
+                    OpenIdIssuer = $"https://login.microsoftonline.com/{tenantId}/v2.0"
+                },
+                Validation = new AzureActiveDirectoryValidationArgs
+                {
+                    AllowedAudiences = 
+                    {
+                        Output.Format($"api://{appRegistration.ClientId}"),
+                        appRegistration.ClientId
+                    }
+                }
+            }
         },
     });
 
     return new Dictionary<string, object?>
     {
-        ["AppName"] = app.Name
+        ["AppName"] = app.Name,
+        ["AppUrl"] = Output.Format($"https://{app.DefaultHostName}")
     };
 });
